a9e00902c351236d1d22aa1b7eb27abc42da6cd3
José Adriano Emmanuel <joseadriano066@gmail.com>
2024-12-05 11:04:57 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Looking to send emails in production? Check out our Email API/SMTP product!
+using System.Net;
+using System.Net.Mail;
+
+var client = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
+{
+    Credentials = new NetworkCredential("5a84d8ec850539", "8e85621418e063"),
+    EnableSsl = true
+};
+client.Send("joseadriano066@gmail.com", "bitmarketao@gmail.com", "Hello world", "testbody");
+
+System.Console.WriteLine("Sent");
