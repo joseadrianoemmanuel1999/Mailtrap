7a2c49c77870822637a05d693b28491916c75a8e
joseadrianoemmanuel1999 <joseadriano066@gmail.com>
2024-12-06 10:28:49 +0100

    First send email

---
+++ MailTest/Domain/MailData.cs
@@ -0,0 +1,9 @@
+namespace MailTest.Domain;
+
+public class MailData
+{
+    public string EmailToId { get; set; }
+    public string EmailToname { get; set; }
+    public string EmailSubject { get; set; }
+    public string EmailBody { get; set; }
+}
--- obj/Debug/net9.0/Mailtrap.AssemblyInfo.cs
+++ obj/Debug/net9.0/Mailtrap.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mailtrap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9e00902c351236d1d22aa1b7eb27abc42da6cd3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mailtrap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mailtrap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
